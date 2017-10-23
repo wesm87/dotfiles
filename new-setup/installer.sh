be0356ae6e216b7953ce32d6227c6d4d3ac3234b
Wes Moberly <github.wes@mailhero.io>
2017-10-23 14:39:51 -0400

    Add Fira Code font to `installer.sh`

--- new-setup/installer.sh
+++ new-setup/installer.sh
@@ -70,6 +70,9 @@ if ! [ -f "$HOME/.vim/bundle/Vundle.vim" ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
+# Install Fira Code font
+brew cask install caskroom/fonts/font-fira-code
+
 # Node
 brew install node
 

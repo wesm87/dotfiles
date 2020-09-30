e9bb33fae1c50f305dce740f14c5da1296c2fb12
Wes Moberly <wmoberly@saec-kv.com>
2020-09-29 23:24:45 -0400

    Add SimpleCLI init params for args / stdin parsing
    - Implement parse_args_mode setting
    - Add new stdin_separator setting
    - Move args parsing logic into a separate SimpleArgsParser class

--- bin/_lib/simple_cli.rb
+++ bin/_lib/simple_cli.rb
@@ -2,112 +2,191 @@
 
 require_relative 'utils'
 
+ANY_QUOTE_REGEXP = /['"]/.freeze
+HAS_SINGLE_QUOTES_REGEXP = /'[^'"]*'/.freeze
+HAS_DOUBLE_QUOTES_REGEXP = /"[^'"]*"/.freeze
+
 # Both stdin and args will be included in parsed args
-PARSE_ARG_MODE_INCLUSIVE = 'INCLUSIVE'
+PARSE_ARGS_MODE_INCLUSIVE = 'INCLUSIVE'
 
 # Use stdin only if it exists, otherwise use args only
-PARSE_ARG_MODE_EXCLUSIVE = 'EXCLUSIVE'
+PARSE_ARGS_MODE_EXCLUSIVE = 'EXCLUSIVE'
+
+STDIN_SEPARATOR_NEWLINE = 'NEWLINE'
+STDIN_SEPARATOR_WHITESPACE = 'WHITESPACE'
+STDIN_SEPARATOR_QUOTES = 'QUOTES'
 
 ###
-# SimpleCLI
+# SimpleArgsParser
+# TODO:1 When parse_args_mode is PARSE_ARGS_MODE_INCLUSIVE, treat stdin / argv
+# TODO:1 as separate lines of input and exclude flags from the argv input line.
+# TODO:2 allow short-hand flags (e.g. -i instead of --ignore-case).
 ###
-class SimpleCLI
-  attr_reader :parse_args_mode
+class SimpleArgsParser
+  attr_reader :parse_args_mode, :stdin_separator
 
-  def initialize(parse_args_mode = PARSE_ARG_MODE_INCLUSIVE)
+  def initialize(
+    parse_args_mode = PARSE_ARGS_MODE_INCLUSIVE,
+    stdin_separator = STDIN_SEPARATOR_QUOTES
+  )
     @parse_args_mode = parse_args_mode
-
-    exit unless args?
+    @stdin_separator = stdin_separator
   end
 
-  def args?
-    stdin_items_and_args.any?
+  def has_args?
+    parsed_args.any?
+  end
+
+  def parsed_args
+    stdin_items_and_args
+  end
+
+  def is_flag_arg?(str)
+    starts_with?('--', str)
+  end
+
+  def is_not_flag_arg?(str)
+    !is_flag_arg?(str)
+  end
+
+  def to_flag_arg(str)
+    is_flag_arg?(str) ? str : "--#{str}"
   end
 
   def argv_args
     @argv_args ||= ARGV.any? ? ARGV : []
   end
 
-  def flag_arg?(str)
-    starts_with?('--', str)
-  end
-
-  def not_flag_arg?(str)
-    !flag_arg?(str)
-  end
-
-  def to_flag_arg(str)
-    flag_arg?(str) ? str : "--#{str}"
-  end
-
-  def flags_list_to_hash(flags)
-    flags.map { |flag| [flag, true] }.to_h
-  end
-
   def stdin_lines
     @stdin_lines ||= parse_stdin_lines
   end
 
+  def trim_and_compact_lines(lines)
+    lines
+      .reject(&:nil?)
+      .map(&:strip)
+      .reject { |line| line.strip.empty? }
+  end
+
+  def get_stdin_line_split_boundary(line)
+    split_by_whitespace = stdin_separator == STDIN_SEPARATOR_WHITESPACE
+    split_by_quotes = stdin_separator == STDIN_SEPARATOR_QUOTES
+
+    return ' ' if split_by_whitespace
+
+    if split_by_quotes && line =~ HAS_DOUBLE_QUOTES_REGEXP
+      '\''
+    elsif split_by_quotes && line =~ HAS_SINGLE_QUOTES_REGEXP
+      '"'
+    else
+      ' '
+    end
+  end
+
+  def split_stdin_line(line)
+    boundary = get_stdin_line_split_boundary(line)
+    split_by_newlines = stdin_separator == STDIN_SEPARATOR_NEWLINE
+    lines_list = split_by_newlines ? [line] : line.split(boundary)
+
+    trim_and_compact_lines(lines_list)
+  end
+
   def parse_stdin_lines
     lines = $stdin.tty? ? [] : $stdin.readlines(chomp: true)
-    lines.map do |line|
-      boundary = line =~ ANY_QUOTE_REGEXP ? ANY_QUOTE_REGEXP : ''
-      line.split(boundary).reject { |item| item.nil? || item.strip.empty? }
-    end
+
+    lines.map { |line| split_stdin_line(line) }
+  end
+
+  def parse_stdin_items_and_args
+    return argv_args if stdin_lines.empty?
+    return stdin_lines.flatten if parse_args_mode == PARSE_ARGS_MODE_EXCLUSIVE
+
+    stdin_lines.map { |line| line + argv_args }.flatten
   end
 
   def stdin_items_and_args
     @stdin_items_and_args ||= parse_stdin_items_and_args
   end
 
-  def parse_stdin_items_and_args
-    if stdin_lines.any?
-      stdin_lines.map { |line| line + argv_args }.flatten
-    else
-      argv_args
-    end
+  def args_groups_hash
+    @args_groups_hash ||= parse_args_groups_hash
+  end
+
+  def parse_args_groups_hash
+    params, flags = partition(:is_not_flag_arg?, stdin_items_and_args)
+
+    {
+      'params' => params,
+      'flags' => flags
+    }
+  end
+
+  def args_params_list
+    @args_params_list ||= args_groups_hash['params']
+  end
+
+  def args_flags_list
+    @args_flags_list ||= args_groups_hash['flags']
+  end
+
+  def args_flags_hash
+    @args_flags_hash ||= args_flags_list.zip(args_flags_list).to_h
   end
 
   def args_hash
-    @args_hash ||= parse_args_hash
+    @args_hash ||= merge_hashes(args_groups_hash, args_flags_hash)
   end
 
-  def parse_args_hash
-    positional, flags = partition(:not_flag_arg?, stdin_items_and_args)
-    flags_hash = flags_list_to_hash(flags)
-    groups_hash = {
-      'positional' => positional,
-      'flags' => flags
-    }
-
-    {}.merge(groups_hash, flags_hash)
+  def get_params
+    args_params_list
   end
 
-  def positional
-    args_hash['positional']
+  def get_flags
+    args_flags_list
   end
 
-  def flag(flag)
-    args_hash[to_flag_arg(flag)]
+  def has_flag(flag)
+    args_hash.has_key?(to_flag_arg(flag))
+  end
+end
+
+###
+# SimpleCLI
+###
+class SimpleCLI
+  attr_reader :args_parser, :parsed_args
+
+  def initialize(
+    parse_args_mode = PARSE_ARGS_MODE_INCLUSIVE,
+    stdin_separator = STDIN_SEPARATOR_QUOTES
+  )
+    @args_parser = SimpleArgsParser.new(parse_args_mode, stdin_separator)
+    @parsed_args = args_parser.parsed_args
+
+    exit unless has_args?
+  end
+
+  def has_args?
+    args_parser.has_args?
+  end
+
+  def get_params
+    args_parser.get_params
+  end
+
+  def get_flags
+    args_parser.get_flags
+  end
+
+  def has_flag(flag)
+    args_parser.has_flag(flag)
   end
 
   def for_each_arg(func = nil, &block)
-    callback = get_callback(block, func)
-
-    return if callback.nil?
-
-    stdin_items_and_args.each do |arg|
-      callback.call(arg)
-    end
+    for_each(func || block, parsed_args)
   end
 
   def puts_for_each_arg(func = nil, &block)
-    callback = get_callback(block, func)
-
-    return if callback.nil?
-
-    for_each_arg do |arg|
-      puts callback.call(arg)
-    end
+    puts_for_each(func || block, parsed_args)
   end
 end
--- bin/_lib/utils.rb
+++ bin/_lib/utils.rb
@@ -1,17 +1,72 @@
 # frozen_string_literal: true
 
-ANY_QUOTE_REGEXP = /['"]/.freeze
+BLANK_STRING_REGEXP = /^[[:space:]]*$/.freeze
+
+def get_callable(func, block = nil)
+  return block if block
+  return method(func) if func.is_a?(String) || func.is_a?(Symbol)
+
+  func&.to_proc || func || nil
+end
 
 def identity(val)
   val.itself
 end
 
-def func?(func = nil)
-  !func.nil? && func.is_a?(Symbol)
+def equals_any?(vals_list, val)
+  vals_list.include?(val)
 end
 
-def get_callback(block, func)
-  func?(func) ? method(func) : block
+def any?(predicate, iterable)
+  callable_method = get_callable(predicate)
+
+  return iterable if callable_method.nil?
+
+  iterable.any? { |item| callable_method[item] }
+end
+
+def any_pass?(predicates_iterable, val)
+  predicates_iterable.any? do |predicate|
+    callable_method = get_callable(predicate)
+
+    return val if callable_method.nil?
+
+    callable_method[val]
+  end
+end
+
+def is_type?(type, val)
+  val.is_a?(type)
+end
+
+def is_any_type?(types, val)
+  !val.nil? && types.any? { |type| val.is_a?(type) }
+end
+
+def is_symbol?(val)
+  is_any_type?([Symbol], val)
+end
+
+def is_string?(val)
+  is_any_type?([String], val)
+end
+
+def is_boolean?(val)
+  equals_any?([true, false], val)
+end
+
+def is_empty?(val)
+  return true if val.nil?
+  return !val if is_boolean?(val)
+  return false if is_any_type?([Numeric, Time], val)
+
+  val.empty?
+end
+
+def is_blank?(val)
+  return is_empty?(val) unless is_string?(val)
+
+  is_empty?(val) || BLANK_STRING_REGEXP.match?(val)
 end
 
 def merge_hashes(hash_a, hash_b)
@@ -35,97 +90,68 @@ def starts_with?(needle, haystack)
 end
 
 def partition(predicate, iterable)
-  iterable.partition { |item| method(predicate).call(item) }
+  callable_method = get_callable(predicate)
+
+  return iterable if callable_method.nil?
+
+  iterable.partition { |item| callable_method[item] }
 end
 
 def str_contains_with?(func, needle, haystack)
-  method(func).call(haystack).include?(method(func).call(needle))
+  callable_method = get_callable(func)
+
+  return false if callable_method.nil?
+
+  needle_result = callable_method[needle]
+  haystack_result = callable_method[haystack]
+
+  haystack_result.include?(needle_result)
 end
 
 def str_contains?(needle, haystack, ignore_case: false)
-  str_contains_with?(ignore_case ? :to_lower_case : :identity, needle, haystack)
+  func = ignore_case ? :to_lower_case : :identity
+
+  str_contains_with?(func, needle, haystack)
 end
 
 def char_count(str)
   str.strip.length
 end
 
+###
+# - @ -> at
+# - & -> and
+# - replace non-alphanumeric characters with spaces
+# - strip leading and trailing spaces
+# - convert whitespace character(s) to a dash
+# - convert to lower-case
+###
 def slugify(str)
-  str.strip
-    .gsub(/[^a-zA-Z0-9]+/, '-')
-    .gsub(/[^a-zA-Z0-9]+$/, '')
-    .gsub(/^[^a-zA-Z0-9]+/, '')
+  str
+    .gsub(/\s*@\s*/, ' at ')
+    .gsub(/\s*&\s*/, ' and ')
+    .gsub(/[^a-zA-Z0-9]+/, ' ')
+    .strip
+    .gsub(/\s+/, '-')
     .downcase
 end
 
-def for_each(callback, iterable)
+def for_each(func, iterable)
+  callable_method = get_callable(func)
+
+  return iterable if callable_method.nil?
+
   iterable.each do |item|
-    method(callback).call(item)
+    callable_method[item]
   end
 end
 
-def puts_for_each(callback, iterable)
+def puts_for_each(func, iterable)
+  callable_method = get_callable(func)
+
+  return iterable if callable_method.nil?
+
   iterable.each do |item|
-    puts method(callback).call(item)
+    puts callable_method[item]
   end
-end
-
-def cli_is_flag_arg?(str)
-  starts_with?('--', str)
-end
-
-def cli_not_flag_arg?(str)
-  !cli_is_flag_arg?(str)
-end
-
-def cli_input_lines
-  ARGV.any? ? ARGV : $stdin.readlines
-end
-
-def cli_args
-  ARGV.any? ? ARGV : []
-end
-
-def puts_for_each_cli_input_line(callback)
-  puts_for_each(callback, cli_input_lines)
-end
-
-def cli_stdin_items_for_line(line)
-  boundary = line =~ ANY_QUOTE_REGEXP ? ANY_QUOTE_REGEXP : ''
-  line.split(boundary).reject! { |item| item.nil? || item.strip.empty? }
-end
-
-def cli_stdin_lines
-  if $stdin.tty?
-    []
-  else
-    result = $stdin
-      .readlines(chomp: true)
-      .map { |line| cli_stdin_items_for_line(line) }
-    puts "cli_stdin_lines: #{result.inspect}"
-    result
-  end
-end
-
-def cli_stdin_items_and_args
-  result = cli_stdin_lines.map { |line| line + cli_args }
-  puts result.inspect
-  result
-end
-
-def cli_args_hash
-  all_args = cli_stdin_items_and_args
-  args_pairs = partition(:cli_not_flag_arg?, all_args)
-
-  puts args_pairs.inspect
-
-  positional, flags = args_pairs
-  result = {
-    'positional' => positional,
-    'flags' => flags
-  }
-
-  puts result
-
-  result
 end

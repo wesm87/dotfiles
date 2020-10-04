6d43e5ce679465f3b4a3f344d84bdad125bd178d
Wes Moberly <wesm87@users.noreply.github.com>
2020-10-04 15:11:54 -0400

    Move screen / vim config files to tilde folder

--- exports/vim.sh
+++ exports/vim.sh
@@ -1,10 +1,6 @@
 # shellcheck shell=bash
 
 function __dotfiles_vim_exports() {
-  # We use single-quotes intentionally to disable variable interpolation
-  # shellcheck disable=2016
-  export VIMINIT='source $MYVIMRC'
-  export MYVIMRC="$HOME/.dotfiles/config/vimrc"
   export EDITOR='vim'
 }
 

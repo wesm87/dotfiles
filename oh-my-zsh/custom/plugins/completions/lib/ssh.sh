a86c65055bfe9cb102290ba0d411f7433a4987d5
Wes Moberly <wmoberly@saec-kv.com>
2020-10-13 18:48:51 -0400

    feat(completions): Add completions for ssh host aliases

---
+++ oh-my-zsh/custom/plugins/completions/lib/ssh.sh
@@ -0,0 +1,13 @@
+# shellcheck shell=bash disable=1090,2128,2207
+
+# Complete ssh with hosts in ~/.ssh/config
+function __dotfiles_completions_ssh_zsh() {
+  zstyle -s ':completion:*:hosts' hosts _ssh_config
+
+  if [[ -r ~/.ssh/config ]]; then
+    _ssh_config+=($(grep -v '\*' ~/.ssh/config | sed -ne 's/Host[=\t ]//p'))
+  fi
+
+  # shellcheck disable=2086
+  zstyle ':completion:*:hosts' hosts $_ssh_config
+}

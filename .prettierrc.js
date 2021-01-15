fa17b1bf963517baba298a597e2d30640329f885
Wes Moberly <wmoberly@saec-kv.com>
2021-01-15 17:05:21 -0500

    feat(installers): update NVM installer to use latest version

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,13 @@
 module.exports = {
-  arrowParens: "avoid",
+  arrowParens: 'avoid',
   bracketSpacing: true,
   jsxBracketSameLine: false,
   jsxSingleQuote: false,
   printWidth: 80,
-  quoteProps: "as-needed",
+  quoteProps: 'as-needed',
   semi: true,
   singleQuote: true,
   tabWidth: 2,
-  trailingComma: "all",
+  trailingComma: 'all',
   useTabs: false,
 };
--- installers/nvm.sh
+++ installers/nvm.sh
@@ -7,7 +7,7 @@ function __dotfiles_installer_nvm() {
   local -r nvm_dir_path="${NVM_DIR:-$nvm_dir_path_default}"
 
   if ! can-source-file "${nvm_dir_path}/nvm.sh"; then
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
   fi
 }
 

c507c45f571013ec1b3d0c6b6977682de1f18320
Wes Moberly <wesm87@users.noreply.github.com>
2022-01-20 18:16:38 -0500

    fix(installers): support both `gpg2` and `gpg` commands for importing RVM keys

--- installers/rvm.sh
+++ installers/rvm.sh
@@ -3,12 +3,21 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+function __dotfiles_gpg_import_keys() {
+  if is-command gpg2; then
+    gpg2 --recv-keys "$@"
+  elif is-command gpg; then
+    gpg --recv-keys "$@"
+  fi
+}
+
 function __dotfiles_installer_rvm() {
   local -r rvm_dir_path="${HOME}/.rvm"
   local -r rvm_script_file_path="${rvm_dir_path}/scripts/rvm"
 
   if ! can-source-file "$rvm_script_file_path"; then
-    gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+    __dotfiles_gpg_import_keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
     curl -sSL https://get.rvm.io | bash -s stable --ruby --ignore-dotfiles
   else
     rvm get stable

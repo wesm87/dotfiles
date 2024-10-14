c922330aa80540d5f0a2f8bc868e648864317663
Wes Moberly <wesm87@users.noreply.github.com>
2024-10-14 10:13:59 -0400

    fix: nvm exports when installed via Homebrew

--- exports/nvm.sh
+++ exports/nvm.sh
@@ -3,10 +3,15 @@
 function __dotfiles_exports_nvm() {
   export NVM_DIR="${HOME}/.nvm"
 
-  local -r nvm_script_file_path="${NVM_DIR}/nvm.sh"
+  local -r nvm_script_file_path_for_brew_install="/opt/homebrew/opt/nvm/nvm.sh"
+  local -r nvm_script_file_path_for_manual_install="${NVM_DIR}/nvm.sh"
 
-  if can-source-file "$nvm_script_file_path"; then
-    source "$nvm_script_file_path"
+  # When NVM is installed via Homebrew, sourcing this script will take care of
+  # setting `NVM_DIR`, linking brew files into `NVM_DIR`, and sourcing `nvm.sh`
+  if can-source-file "$nvm_script_file_path_for_brew_install"; then
+    source "$nvm_script_file_path_for_brew_install"
+  elif can-source-file "$nvm_script_file_path_for_manual_install"; then
+    source "$nvm_script_file_path_for_manual_install"
   fi
 }
 
